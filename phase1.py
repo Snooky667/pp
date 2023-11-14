aaa45a59addf0fad8dca710af1ac1304a9403370
Guillaume Laroche <guillaume.laroche.5@ulaval.ca>
2023-11-14 16:49:53 -0500

    Instaurer arg pass

--- phase1.py
+++ phase1.py
@@ -4,6 +4,11 @@ import json
 from datetime import timedelta
 import datetime
 
+
+#. Création des todaydate pour default input user in -d & -f
+today_date = datetime.date.today()
+formatted_date = today_date.strftime("%Y-%m-%d")
+
 def analyser_commande():
     parser = argparse.ArgumentParser(description='Extraction de valeurs historiques pour un ou plusieurs symboles boursiers.') #. Symboles est une liste
     #. liste des options disponibles
@@ -13,8 +18,19 @@ def analyser_commande():
     
     
     parser.add_argument('-d', '--debut', help='Date recherchée la plus ancienne (format: AAAA-MM-JJ)', default=None)
-    parser.add_argument('-f', '--fin', help='Date recherchée la plus récente (format: AAAA-MM-JJ)')
+    parser.add_argument('-f', '--fin', help='Date recherchée la plus récente (format: AAAA-MM-JJ)', default=formatted_date)
     parser.add_argument('-v', '--valeur',choices=['fermeture', 'ouverture', 'min', 'max', 'volume'], help=' La valeur désirée (par défaut: fermeture)', default='fermeture')
 
-    args = parser.parse_args()
-    print(args)
+    args = parser.parse_args()     
+
+    if args.debut is None:          #. Dans le cas ou on met pas de date de début
+        args.debut = args.fin       #. debut = fin
+    return args
+
+
+def produire_historique(symbole, debut, fin, valeur):         #. Fonction pour produire lhistorique
+    url = f'https://pax.ulaval.ca/action/{symbole}/historique/' #. Grement que le prof a donner
+    params = {                                                  #. Aucune calisse d'idée comment sa marche
+    'début': debut,
+    'fin': fin,
+    }
